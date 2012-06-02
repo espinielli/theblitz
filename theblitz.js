41b421e70484913797e6e88f44ad6810a5ded200
Enrico Spinielli <enrico.spinielli@gmail.com>
2012-06-02 14:47:49 +0200

    typo and better time shift. plus cleanup

--- theblitz.js
+++ theblitz.js
@@ -4,23 +4,24 @@ var thespot = {"lat": 51.49463582758311, "lon": -0.04628918457023179 }
 
 
 // the first day of the blitz started after 16:44, the time
-// German airplanes entered British airspance.
+// German airplanes entered British airspace.
 //
 // The data entries should be shifted accordingly and the start of
 // the animation should be minute 1004, the first drop at 16:57 will
 // be 13 minutes later (data[84]) and continue into next day:
-// time=h:m
-// minutes from start of raid: (h*60+m - 1004) % 1440
+// datum.time=h:m
+// hence minutes from start of raid: (h*60+m - 1004) % 1440
 var minutes = function(d) {
   // transform a time of day string [H]H:MM, i.e. "1:08"
   // in minutes from midnight, i.e. 68.
   reggie = /(\d):(\d{2})/g;
-  t = reggie.exec(d.value.time);
+  t = reggie.exec(d.time);
   t = parseInt(t[1]) * 60 + parseInt(t[2]);
   // shift so that 16:44 is the start of time
   // (1440 = 24 * 60, the number of minutes in 1 day)
-  t = (2*(t + 1440 - 1004)) % 1440;
-  return 20 * t + 30;
+  t = (((t - 1004 ) % 1440) + 600 + 1440) % 1440;
+  // console.log("" + d.value.order + " ("+ d.value.time + ") ->" + t);
+  return t;
 };
 
 // Create the Google Map
@@ -44,9 +45,11 @@ d3.json("drops.json", function(data) {
         overlay.draw = function() {
             var projection = this.getProjection(),
                 padding = 10;
-
+          function key(d) {
+            return d.order;
+          }
             var marker = layer.selectAll("svg")
-                .data(d3.entries(data))
+            .data(data, key)
                 .each(transform) // update existing markers
                     .enter().append("svg:svg")
                 .each(transform)
@@ -58,7 +61,7 @@ d3.json("drops.json", function(data) {
                 .attr("cx", padding)
                 .attr("cy", padding)
               .transition()
-            .delay(minutes)
+            .delay(function(d) {return 20 * minutes(d) + 30;})
                 .duration(2000)
                 .style("fill", "black");;
 
@@ -70,7 +73,7 @@ d3.json("drops.json", function(data) {
             //     .text(function(d) { return d.value.time; });
 
             function transform(d) {
-                d = new google.maps.LatLng(d.value.location.lat, d.value.location.lon);
+                d = new google.maps.LatLng(d.location.lat, d.location.lon);
                 d = projection.fromLatLngToDivPixel(d);
                 return d3.select(this)
                     .style("left", (d.x - padding) + "px")
